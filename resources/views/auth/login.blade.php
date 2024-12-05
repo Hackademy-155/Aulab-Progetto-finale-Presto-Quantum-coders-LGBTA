77e2b69b4f7b4289d6bf2cdffc9606da1fd95c6e
Alessandro Taloni <alessandrotalo99@gmail.com>
2024-12-05 15:35:51 +0100

    funzioni login e register

---
+++ resources/views/auth/login.blade.php
@@ -0,0 +1,10 @@
+<x-layout>
+    <div class="container">
+        <div class="row">
+            <div class="col-12">
+                <h1>Accedi</h1>
+            </div>
+        </div>
+    </div>
+    
+</x-layout>
