32dcf9de1b5173b7b6ad1df470c4cb7994845ef6
Giovanni Loiacono <giovanni.loiacono182@gmail.com>
2024-12-06 19:59:18 +0100

    scritte homepage, modifiche varie, animazioni fatte

--- resources/views/components/navbar.blade.php
+++ resources/views/components/navbar.blade.php
@@ -1,36 +1,35 @@
 <nav class="navbar navbar-expand-lg p-3 fixed-top navBar">
     <div class="container-fluid">
-
         <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent"
         aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
         <span class="navbar-toggler-icon"></span>
     </button>
     <div class="collapse navbar-collapse d-md-flex justify-content-md-evenly align-items-md-center" id="navbarSupportedContent">
         <ul class="navbar-nav">
-            <li class="nav-item">
-                <a class="nav-link active coloreTasti" aria-current="page" href="{{ route('home') }}">Home</a>
+            <li class="nav-item g-5 px-3">
+                <a class="nav-link active" aria-current="page" href="{{ route('home') }}">Home</a>
             </li>
-            <li class="nav-item">
-                <a class="nav-link active coloreTasti" aria-current="page" href="{{ route('product.index') }}">I nostri prodotti</a>
+            <li class="nav-item g-5 px-3">
+                <a class="nav-link active" aria-current="page" href="{{ route('product.index') }}">I nostri prodotti</a>
             </li>
 
             @auth
-            <li class="nav-item" aria-current="page">
-                <a class="nav-link active coloreTasti"  href="{{route('product.create')}}">Aggiungi prodotto</a>
+            <li class="nav-item g-5 px-3" aria-current="page">
+                <a class="nav-link active"  href="{{route('product.create')}}">Aggiungi prodotto</a>
             </li>
             @endauth
         </ul>
 
         @guest
-        <ul class="navbar-nav ">
-            <li class="nav-item dropdown ">
-                <a class="nav-link coloreTasti dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown"
+        <ul class="navbar-nav">
+            <li class="nav-item dropdown active ">
+                <a class="nav-link active dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown"
                 aria-expanded="false">
                 Accedi/Registrati
             </a>
             <ul class="dropdown-menu">
-                <li><a class="dropdown-item coloreTasti" href="{{route('login')}}">Accedi</a></li>
-                <li><a class="dropdown-item coloreTasti" href="{{route('register')}}">Registrati</a></li>
+                <li><a class="dropdown-item active" href="{{route('login')}}">Accedi</a></li>
+                <li><a class="dropdown-item active" href="{{route('register')}}">Registrati</a></li>
             </ul>
         </li>
     </ul>
@@ -55,7 +54,6 @@
     </li>
 </ul>
 @endauth
-
 </div>
 </div>
 </nav>
