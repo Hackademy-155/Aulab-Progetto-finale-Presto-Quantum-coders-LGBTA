0164396b88506b81f90045d55a229b0fe209dfa8
Giovanni Loiacono <giovanni.loiacono182@gmail.com>
2024-12-13 23:21:50 +0100

    fix vari ai caroselli

--- resources/views/livewire/product-create.blade.php
+++ resources/views/livewire/product-create.blade.php
@@ -7,70 +7,72 @@
                 <h2 class="fw-bold text-center mb-5 title-insert-product display-4">
                     {{ __('ui.Aggiungi il tuo prodotto') }}</h2>
 
-                    <span class="input-span mb-3">
-                        <label for="title" class="label">{{ __('ui.Titolo') }}</label>
-                        <input type="text" id="title" wire:model.live.blur="title">
-                        @error('title')
+                <span class="input-span mb-3">
+                    <label for="title" class="label">{{ __('ui.Titolo') }}</label>
+                    <input type="text" id="title" wire:model.live.blur="title">
+                    @error('title')
                         <span class="alert alert-danger">{{ $message }}</span>
-                        @enderror
-                    </span>
+                    @enderror
+                </span>
 
-                    <span class="input-span mb-3">
-                        <label for="category" class="label">{{ __('ui.Categoria') }}</label>
-                        <select id="category" wire:model="category" class="form-select mb-3 custom-select">
-                            <option value="" disabled selected>{{ __('ui.Seleziona la categoria') }}</option>
-                            @foreach ($categories as $category)
+                <span class="input-span mb-3">
+                    <label for="category" class="label">{{ __('ui.Categoria') }}</label>
+                    <select id="category" wire:model="category" class="form-select mb-3 custom-select">
+                        <option value="" disabled selected>{{ __('ui.Seleziona la categoria') }}</option>
+                        @foreach ($categories as $category)
                             <option class="custom-option" value="{{ $category->id }}">{{ __('ui.' . $category->name) }}
                             </option>
-                            @endforeach
-                        </select>
-                        @error('category')
+                        @endforeach
+                    </select>
+                    @error('category')
                         <span class="alert alert-danger">{{ $message }}</span>
-                        @enderror
-                    </span>
+                    @enderror
+                </span>
 
-                    <span class="input-span mb-3">
-                        <label for="price" class="label">{{ __('ui.Prezzo') }}</label>
-                        <input type="number" id="price" wire:model.live.blur="price">
-                        @error('price')
+                <span class="input-span mb-3">
+                    <label for="price" class="label">{{ __('ui.Prezzo') }}</label>
+                    <input type="number" id="price" wire:model.live.blur="price">
+                    @error('price')
                         <span class="alert alert-danger">{{ $message }}</span>
-                        @enderror
-                    </span>
+                    @enderror
+                </span>
 
-                    <span class="input-span mb-3">
-                        <label for="description" class="label">{{ __('ui.Descrizione') }}</label>
-                        <textarea type="text" name="description" id="description" wire:model.live.blur="description"></textarea>
-                        @error('description')
+                <span class="input-span mb-3">
+                    <label for="description" class="label">{{ __('ui.Descrizione') }}</label>
+                    <textarea type="text" name="description" id="description" wire:model.live.blur="description"></textarea>
+                    @error('description')
                         <span class="alert alert-danger">{{ $message }}</span>
-                        @enderror
-                    </span>
-                    @if (session('success'))
+                    @enderror
+                </span>
+                @if (session('success'))
                     <div class="alert alert-success">
                         {{ session('success') }}
                     </div>
-                    @endif
-                    <div class="mb-3">
-                        <input type="file" id="fileInput" wire:model.live="temporary_images" multiple
+                @endif
+                <div class="mb-3">
+                    <input type="file" id="fileInput" wire:model.live="temporary_images" multiple
                         class="form-control shadow @error('temporary_images.*') is-invalid @enderror"
                         placeholder="Img/">
 
-                        @error('temporary_images.*')
+                    @error('temporary_images.*')
                         <p class="fst-italic text-danger">{{ $message }} </p>
-                        @enderror
-                        @error('temporary_images')
+                    @enderror
+                    @error('temporary_images')
                         <p class="fst-italic text-danger">{{ $message }} </p>
-                        @enderror
-                    </div>
+                    @enderror
+                </div>
 
-                    @if (!empty($images))
+                @if (!empty($images))
                     <div class="row">
                         <col class="12">
                         <p>Photo Preview</p>
                         <div class="row border-4 border-success rounded py-4 shadow">
-                            @foreach ($images as $key=> $image)
-                                <div class="col-10 d-flex flex-column align-items-center my-3">
-                                    <div class="img-preview mx-auto shadow rounded" style="background-image: url({{ $image->temporaryUrl() }});">
-                                        <button type="button" class="btn btn-danger mt-1" wire:click="removeImage({{ $key}})">
+                            @foreach ($images as $key => $image)
+                                <div class="col-6 d-flex flex-column align-items-center my-3">
+                                    <div class="img-preview mx-auto shadow rounded"
+                                        style="background-image: url({{ $image->temporaryUrl() }});">
+                                        <button type="button" class="btn btn-danger mt-1"
+                                            wire:click="removeImage({{ $key }})">
                                             X
                                         </button>
                                     </div>
--- resources/views/products/product-show.blade.php
+++ resources/views/products/product-show.blade.php
@@ -4,7 +4,8 @@
             <div class="col-12 mt-5">
 
                 <h2 class="text-center display-4 fw-bold">
-                    <a class="display-5 text-decoration-none color-second me-5" href="{{ route('product.index') }}"><i class="bi bi-arrow-left-circle-fill"></i></a>{{ $product->title }} - {{__('ui.Dettagli')}}
+                    <a class="display-5 text-decoration-none color-second me-5" href="{{ route('product.index') }}"><i
+                            class="bi bi-arrow-left-circle-fill"></i></a>{{ $product->title }} - {{ __('ui.Dettagli') }}
                 </h2>
             </div>
         </div>
@@ -12,37 +13,40 @@
         <div class="row justify-content-between align-items-center my-5 bg-body-secondary p-5 rounded-3 shadow">
 
             <div class="col-12 col-md-5">
-                @if ($product->images->count()>0)
-                <div id="productCarousel" class="carousel slide shadow rounded overflow-hidden" data-bs-ride="carousel">
-                    <div class="carousel-inner">
-                        @foreach ($product->images as $key=> $image)
-                        <div class="carousel-item @if($loop->first) active @endif">
-                            <img src="{{Storage::url($image->path)}}" class="d-block w-100 zoom-on-hover"
-                            alt="Immagine {{$key +1}} dell'articolo {{$product->title}}">
+                @if ($product->images->count() > 0)
+                    <div id="productCarousel" class="carousel slide shadow rounded overflow-hidden"
+                        data-bs-ride="carousel">
+                        <div class="carousel-inner">
+                            @foreach ($product->images as $key => $image)
+                                <div class="carousel-item @if ($loop->first) active @endif">
+                                    <img src="{{ Storage::url($image->path) }}" class="d-block w-100 zoom-on-hover"
+                                        alt="Immagine {{ $key + 1 }} dell'articolo {{ $product->title }}">
+                                </div>
+                            @endforeach
                         </div>
-                        @endforeach
+                        @if ($product->images->count() > 1)
+                            <button class="carousel-control-prev" type="button" data-bs-target="#productCarousel"
+                                data-bs-slide="prev">
+                                <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+                            </button>
+                            <button class="carousel-control-next" type="button" data-bs-target="#productCarousel"
+                                data-bs-slide="next">
+                                <span class="carousel-control-next-icon" aria-hidden="true"></span>
+                            </button>
+                        @endif
                     </div>
-                    @if ($product->images->count()>1)
-
-                    <button class="carousel-control-prev" type="button" data-bs-target="#productCarousel" data-bs-slide="prev">
-                        <span class="carousel-control-prev-icon" aria-hidden="true"></span>
-                    </button>
-                    <button class="carousel-control-next" type="button" data-bs-target="#productCarousel" data-bs-slide="next">
-                        <span class="carousel-control-next-icon" aria-hidden="true"></span>
-                    </button>
-                    @endif
-                </div>
                 @else
-                <img src="https://picsum.photos/300" alt="Nessuna foto inserita dall'utente">
+                    <img src="https://picsum.photos/300" alt="Nessuna foto inserita dall'utente">
                 @endif
             </div>
 
             <div class="col-12 col-md-6 text-center">
-                <p class="description mt-5 mt-md-0"><strong>{{ __('ui.Descrizione')}}</strong> {{ $product->description }}</p>
+                <p class="description mt-5 mt-md-0"><strong>{{ __('ui.Descrizione') }}</strong>
+                    {{ $product->description }}</p>
                 <div class="d-flex justify-content-around mt-4">
-                    <p class="small fw-bold">{{ __('ui.Prezzo')}}: {{ $product->price }}$</p>
+                    <p class="small fw-bold">{{ __('ui.Prezzo') }}: {{ $product->price }}$</p>
                     <a href="{{ route('filterByCategory', $product->category) }}" class="text-decoration-none">
-                        <p class="small">{{ __('ui.' . $product->category->name)  ?? 'ui.' . 'Sconosciuta' }}</p>
+                        <p class="small">{{ __('ui.' . $product->category->name) ?? 'ui.' . 'Sconosciuta' }}</p>
                     </a>
 
                 </div>
