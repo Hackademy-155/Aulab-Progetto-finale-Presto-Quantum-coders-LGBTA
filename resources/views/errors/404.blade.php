ba48a14de482ae966374afad3d69e7711b374c23
Giovanni Loiacono <giovanni.loiacono182@gmail.com>
2024-12-15 23:09:28 +0100

    creata pagina 404, fix e aggiunte  traduzioni

---
+++ resources/views/errors/404.blade.php
@@ -0,0 +1,13 @@
+<x-layout>
+<x-navbar/>
+<header class="mt-5">
+    <div class="row">
+        <div class="col-12 mt-5 d-flex align-items-center justify-content-center flex-column">
+            <h2 class="text-dark text-center">{{ __('ui.Not Found')}} 404</h2>
+            <img src="\media\sticker_2119-512x512.png" alt="">
+        </div>
+    </div>
+
+</header>
+
+</x-layout>
